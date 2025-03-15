81609f609389fcb2521ea88171de43f216ada04a
Romano Kanters <info@klarifai.nl>
2025-03-15 13:36:14 +0100

    Update Jest and Babel configuration for better compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1'
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   }
 };
